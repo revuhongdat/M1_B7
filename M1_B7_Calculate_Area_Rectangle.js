969bcba7b2cebb1f3bf59bd91c06d956e575afe6
DatVu <revuhongdat@gmail.com>
2023-07-11 13:35:22 +0700

    M1-B7

---
+++ M1_B7_Calculate_Area_Rectangle.js
@@ -0,0 +1,7 @@
+inputWidth = prompt('Enter Width');
+inputHeight = prompt('Enter Height');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write('The area is:' + area);
+
